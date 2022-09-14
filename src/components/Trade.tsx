257175cabf1a1990ec25ba9eb65c2048f01df5a9
Evan Brierton <35705911+evanbrierton@users.noreply.github.com>
2022-09-14 12:45:25 +0100

    Implement currency selector

--- src/components/Trade.tsx
+++ src/components/Trade.tsx
@@ -1,3 +1,5 @@
+import Chevron from "./Chevron";
+
 const Trade = () => {
   const renderBuySellButtons = () => (
     <div className="flex gap-2 justify-start items-center">
@@ -6,7 +8,7 @@ const Trade = () => {
       </button>
       <button
         type="button"
-        className="font-medium text-gray-4 border-b-2 border-b-transparent"
+        className="font-medium text-gray-4 hover:text-black border-b-2 border-b-transparent"
       >
         Sell
       </button>
@@ -28,13 +30,13 @@ const Trade = () => {
       <div className=" flex gap-1 justify-between items-center text-[14px]">
         <button
           type="button"
-          className="w-[32px] h-[32px] bg-gray-8 rounded-full"
+          className="w-[32px] h-[32px] bg-gray-8 hover:bg-gray-7 rounded-full"
         >
           C
         </button>
         <button
           type="button"
-          className="[py-[6px] px-[12px] w-[55px] h-[32px] bg-gray-8 rounded-full"
+          className="px-[12px] w-[55px] h-[32px] bg-gray-8  hover:bg-gray-7 rounded-full"
         >
           MAX
         </button>
@@ -43,8 +45,43 @@ const Trade = () => {
   );
 
   const renderCurrencySelect = () => {
+    const renderRow = (
+      label: string,
+      currency: string,
+      { first = false, last = false } = {},
+      action?: () => void
+    ) => (
+      <div
+        className={`${action ? "hover:bg-gray-8 cursor-pointer" : ""} ${
+          first ? "rounded-t-xl" : ""
+        } ${
+          last ? "rounded-b-xl" : ""
+        } flex justify-between items-center px-4 w-[100%] h-[50%]`}
+      >
+        <div className="flex justify-between items-center w-[50%]">
+          <span className="w-12 text-caption">{label}</span>
+          <div className="flex gap-1 justify-between items-center">
+            <div className="w-5 h-5 bg-white rounded-full border border-gray-6" />
+            <span>{currency}</span>
+          </div>
+        </div>
+        <button type="button" className="rounded-full">
+          <Chevron />
+        </button>
+      </div>
+    );
+
     return (
-      <div className="w-[330px] h-[131px] rounded-xl border border-gray-6" />
+      <div className="flex flex-col justify-around w-[330px] h-[131px] rounded-xl border border-gray-6">
+        {renderRow("Buy", "BTC", { first: true }, () => {})}
+        <hr className="m-0 text-gray-6" />
+        <button
+          type="button"
+          aria-label="swap"
+          className="absolute ml-[235px] w-8 h-8 bg-gray-8 hover:bg-gray-7 rounded-full border-[3px] border-white"
+        />
+        {renderRow("Pay With", "ETH", { last: true })}
+      </div>
     );
   };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@
  * @component
  */
 
+import Chevron from "@/components/Chevron";
 import Navbar from "@/components/Navbar";
 import Trade from "@/components/Trade";
 
@@ -15,19 +16,7 @@ const Homepage = () => {
         <nav className="flex" aria-label="Breadcrumb">
           <ol className="flex gap-2 items-center my-6 desktop:mb-8">
             <li>All Assets</li>
-            <svg
-              width="9"
-              height="16"
-              viewBox="0 0 9 16"
-              className="shrink-0"
-              fill="none"
-              stroke="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Chevron</title>
-              <path d="M1 1L8 8L1 15" strokeLinecap="round" />
-            </svg>{" "}
-            <li>Bitcoin</li>
+            <Chevron /> <li>Bitcoin</li>
           </ol>
         </nav>
 
